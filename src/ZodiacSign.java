6028ebad173d5e6d08c37a2506b2536069f0a826
kylielent <lentk@student.swosu.edu>
2015-10-19 09:51:24 -0500

    added astrological signs

---
+++ src/ZodiacSign.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class ZodiacSign {
+		public static void main(String[] args) {
+			Scanner input = new Scanner (System.in);
+			
+				System.out.print("Enter the year you were born: ");
+				int year = input.nextInt();
+				
+				System.out.print("Enter the number of month you were born: ");
+				int month = input.nextInt();
+				
+				System.out.println("Enter the day you were born on: ");
+				int day = input.nextInt();
+				
+				System.out.print("You were born in the year of the ");
+				
+				switch (year % 12) {
+			
+				case 0: System.out.println("monkey.");
+				case 1:  System.out.println("rooster.");    break;
+				case 2:  System.out.println("dog.")    ;    break;
+				case 3:  System.out.println("pig.")    ;    break;
+				case 4:  System.out.println("rat.")    ;    break;
+				case 5:  System.out.println("ox.")     ;    break;
+				case 6:  System.out.println("tiger.")  ;    break;
+				case 7:  System.out.println("rabbit.") ;    break;
+				case 8:  System.out.println("dragon.") ;    break;
+				case 9:  System.out.println("snake.")  ;    break;
+				case 10: System.out.println("horse.")  ;    break;
+				case 11: System.out.println("sheep.")  ;    break;
+				case 12: System.out.println("");
+				}
+				System.out.print("You are an ");
+				
+				switch (month){
+				case 0:
+				case 1:  System.out.println("aquarius.");    break;
+				case 2:  System.out.println("pisces.")    ;    break;
+				case 3:  System.out.println("pig.")    ;    break;
+				case 4:  System.out.println("rat.")    ;    break;
+				case 5:  System.out.println("ox.")     ;    break;
+				case 6:  System.out.println("tiger.")  ;    break;
+				case 7:  System.out.println("rabbit.") ;    break;
+				case 8:  System.out.println("dragon.") ;    break;
+				case 9:  System.out.println("snake.")  ;    break;
+				case 10: System.out.println("horse.")  ;    break;
+				case 11: System.out.println("sheep.")  ;    break;
+				case 12: System.out.println("");
+				
+				}
+				}
+		}
+
---
+++ src/ZodiacSigns.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAB
+ *
+ */
+public class ZodiacSigns {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
